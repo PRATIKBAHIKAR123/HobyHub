b41ead49ce57d41048e9f0d16a46f5e5baf15f0e
Pratik Bahikar <pratik@clearconceptsolutions.com>
2025-04-17 11:48:18 +0530

    vendor service

--- services/vendorService.ts
+++ services/vendorService.ts
@@ -1,6 +1,18 @@
 import { API_BASE_URL } from "@/lib/apiConfigs";
 import axios from "axios";
+import { getStoredToken } from "@/utils/localStorage";
+import { VendorClassData } from "@/app/services/vendorService";
 
+const getHeaders = () => ({
+  "Content-Type": "application/json",
+  Authorization: `Bearer ${getStoredToken()}`,
+});
+
+/**
+ * Get all activities with optional filters
+ * @param vendorId Optional filters to apply to the activities
+ * @returns List of filtered activities
+ */
 
 export const registerVendor = async (formData: {}) => {
     try {
@@ -11,4 +23,65 @@ export const registerVendor = async (formData: {}) => {
       console.error("Error:", error);
       return error.response;
     }
+};
+
+export const getClassList = async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/vendor/vendor-class/get-all?id=0`, {
+      headers: getHeaders(),
+    });
+    console.log("Response:", response.data);
+    return response;
+  } catch (error : any) {
+    console.error("Error:", error);
+    return error.response;
+  }
+};
+
+export const createClass = async (formData:VendorClassData[]) => {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/vendor/vendor-class/create`,formData,{ headers: getHeaders()});
+    console.log("Response:", response.data);
+    return response;
+  } catch (error : any) {
+    console.error("Error:", error);
+    return error.response;
+  }
+};
+
+export const createCourse = async (formData:VendorClassData[]) => {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/vendor/vendor-course/create`,formData,{ headers: getHeaders()});
+    console.log("Response:", response.data);
+    return response;
+  } catch (error : any) {
+    console.error("Error:", error);
+    return error.response;
+  }
+};
+
+export const getCourseList = async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/vendor/vendor-course/get-all?id=0`, {
+      headers: getHeaders(),
+    });
+    console.log("Response:", response.data);
+    return response;
+  } catch (error : any) {
+    console.error("Error:", error);
+    return error.response;
+  }
+};
+
+export const getImageList = async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/vendor/vendor-file/get-all?id=0`, {
+      headers: getHeaders(),
+    });
+    console.log("Response:", response.data);
+    return response;
+  } catch (error : any) {
+    console.error("Error:", error);
+    return error.response;
+  }
 };
