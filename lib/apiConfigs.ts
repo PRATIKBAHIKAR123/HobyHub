49517832129539f45b807ec67aa7b158d04e2d8a
Mohammed <mohammedjuzer5252@gmail.com>
2025-05-20 12:52:14 +0530

    api fix for local

--- lib/apiConfigs.ts
+++ lib/apiConfigs.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL_1 = "https://api.hobyhub.com/";
-export const API_BASE_URL = "https://api.hobyhub.com/api/1";
+export const API_BASE_URL_1 = "https://localhost:55288/";
+export const API_BASE_URL = "https://localhost:55288/api/1";
 //export const GOOGLE_MAP_API_KEY = "AIzaSyBiXRza3cdC49oDky7hLyXPqkQhaNM4yts";
-export const GOOGLE_MAP_API_KEY = "AIzaSyBu5_sJSqh-SG1-GjuFsbG6KVuNzs6vuSA";
+export const GOOGLE_MAP_API_KEY = "AIzaSyBR0YNTIOqZVKtIDG0Pjf-bkrJke_DtfBs";
